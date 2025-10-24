5eb942a8ae37b7dbb79afa996df12af742cd47ed
CARLOS HEITOR GUIMARAES PEREIRA DO LAGO <carlos.lago@aluno.senac.rio>
2025-10-24 18:30:41 -0300

    conta bancaria ok

--- exercicio3.py
+++ exercicio3.py
@@ -1,39 +1,43 @@
 class ContaBancaria:
-    def __init__(self, titular,saldo):
-        self.titular=titular
-        self.__saldo=saldo
 
-    def depositar(self,valor):
-        self.__saldo+=valor
+    def __init__(self):
+        self.titular = ""
+        self.__saldo = 0
 
     def sacar(self, valor):
-        if valor<=self.__saldo:
+        print(f"valor é: R$ {valor} e o saldo é {self.__saldo}")
+        if self.__saldo>=valor:
             self.__saldo-=valor
+            print(f"O saque foi de R$ {valor}. O saldo atual é R${self.__saldo}")
         else:
             print("saldo insuficiente.")
-    
-    def mostrar__saldo(self):
-        print(f"Saldo atual: R$ {self.__saldo}")
-    
-titular=input("qual o seu nome? ")
-print("1. saque", "2. deposito")
-op=input(f" {titular} o que voce deseja fazer?")
 
-if op=="1":
-    def sacar(self, valor):
-        valor=input(f"{titular}, Quanto vôce deseja sacar ?")
-        def sacar(self, valor):
-            if valor>=self.__saldo:
-                self.__saldo-=valor
-            else:
-                print("saldo insuficiente.")
-
-elif op=="2":
-    saldo=input("quanto você deseja depositar? ")
     def depositar(self,valor):
         self.__saldo+=valor
-        
 
-titular=ContaBancaria(titular,saldo)
-print(f"{titular}, o seu saldo inicial é de R$ {saldo}")
+    def mostrar_saldo(self):        
+        if(self.__saldo == None):
+            print("Seu saldo está zerado")
+        else:
+            print(f"Saldo atual: R$ {self.__saldo}")
 
+print("-"*30)
+cliente = ContaBancaria()
+
+cliente.titular = input('Digite o seu nome: ')
+print(f"Seja bem-vindo(a) {cliente.titular}! O sue saldo atual é {cliente.mostrar_saldo()} ")
+
+
+print("1. saque", "2. deposito, 3. saldo")
+op=int(input(f" {cliente.titular} o que voce deseja fazer?"))
+
+if(op==1):
+    valor = float(input("Digite o valor a ser sacado: "))
+    resultado = cliente.sacar(valor)
+    
+elif(op==2):
+    valor = float(input("Digite o valor a ser depositado: "))
+    resultado = cliente.depositar(valor)
+    print(resultado)
+elif(op==3):
+    print(f"Seu saldo atual é: R${cliente.mostrar_saldo()}")
